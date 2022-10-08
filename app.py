d22b6e0f0e83a85a271ccd156c3be01133869f33
Indrajeet Singh <indrajeetsinghmaan@gmail.com>
2022-10-08 16:54:58 +0530

    added hot-reload in docker

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from flask import Flask, request
-
+import uuid
+from db import items
 
 app = Flask(__name__)
 
-items = [
-        {
-            "name": "Green Apple Mojito",
-            "price": 160
-        },
-        {
-            "name": "Momos",
-            "price": 60
-        }
-]
+
+
 
 
 @app.get('/items')     
@@ -21,34 +14,44 @@ def get_items():
 
 @app.get('/item')     
 def get_item():
-    name = request.args.get('name')
-    for item in items:
-        if name == item['name']:
-            return item
-    return {'message': "Record doesn't exist"}, 404
+    id = request.args.get('id')
+    try:
+        return items[id]
+    except KeyError:
+        return {'message': "Record doesn't exist"}, 404
 
 @app.post('/item')
 def add_item():
     request_data = request.get_json()
-    items.append(request_data)
+    if "name" not in request_data or "price" not in request_data:
+        return {"message":"'name' and 'price' must be included in body"}, 400
+    items[uuid.uuid4().hex] = request_data
     return {"message": "Item added succesfully"}, 201
 
 
 @app.put('/item')
 def update_item():
-    request_data = request.get_json()
-    for item in items:
-        if item['name'] == request_data['name']:
-            item['price'] = request_data['price']
-            return {"message": "Item updated succesfully"}
-    return {'message': "Given Record doesn't exists"}, 404
+    
+    id = request.args.get('id')
+    if id == None:
+        return {"message":"Given id not found"}, 404
+        
+
+    if id in items.keys():
+        request_data = request.get_json()
+        if "name" not in request_data or "price" not in request_data:
+            return {"message":"'name' and 'price' must be included in body"}, 400
+        items[id] =  request.get_json()
+        return {'message': "Item updated successfully"}
+    return {'message': " Item Not Found"}, 404
 
 
 @app.delete('/item')     
 def delete_item():
-    name = request.args.get('name')
-    for item in items:
-        if name == item['name']:
-            items.remove(item)
-            return {'message': 'Item deleted successfully'}
+    id = request.args.get('id')
+    if id == None:
+        return {"message":"Given id not found"}, 404
+    if id in items.keys():
+        del items[id]
+        return {'message': 'Item deleted successfully'}
     return {'message': "Record doesn't exist"}, 404
---
+++ db.py
@@ -0,0 +1,10 @@
+items = {
+        "2653e30f28864669909246e15d4c8c62":{
+                                            "name": "Green Apple Mojito",
+                                            "price": 160
+                                           },
+        "a7205bc2177a4894bee78ef1e9c49607":{
+                                            "name": "Momoss",
+                                            "price": 60
+                                            }
+}
